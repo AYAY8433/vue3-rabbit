43afa7eb87c82d7e5d48ff92dcc3b637e8a17137
安宇 <8305413+an-astronaut@user.noreply.gitee.com>
2025-02-18 12:45:50 +0800

    axios技术封装

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {getCategory} from '@/apis/testApi'
+import { onMounted, ref } from 'vue';
+onMounted(() =>{
+  getCategory().then((res) =>{
+   console.log(res);
+   
+  })
+})
 </script>
 
 <template>
-  <header>
-    <el-button type="primary">哈哈</el-button>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+   <el-button type="primary">哈哈</el-button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
